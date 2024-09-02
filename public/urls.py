a596c5c5adbce17fc59bb7b5b7aebf921f14d6e2
fbdav <brad.davison@novamacro.net>
2024-09-02 12:18:32 -0500

    Updated urls and views for about.html

--- public/urls.py
+++ public/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('/about', views.about, name='about' )
+    path('about/', views.about, name='about' )
 ]
 
 # if settings.DEBUG:

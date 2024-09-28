c397b9e535c5bc2a2e0798c62362e26e0aa35ed0
fbdav <brad.davison@novamacro.net>
2024-09-28 11:36:46 -0500

    Updated Added Patient resources to urls, views and models

--- library/views.py
+++ library/views.py
@@ -59,4 +59,4 @@ def patient_resources(request):
         path = 'https://player.vimeo.com/video/'+ link[3] +'?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:60%;height:60%;" title=' + name
         info.append({'name': name, 'path': path})
     context = {'title': title}
-    return render(request, 'patient-resources.html', context())
+    return render(request, 'patient-resources.html', context)

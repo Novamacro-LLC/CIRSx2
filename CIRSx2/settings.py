cc8d5a0bc13715a144aa1dfcb898a6e46a952928
fbdavison <80009345+fbdavison@users.noreply.github.com>
2025-07-13 20:43:00 -0500

    Added a loading modal for the search.

--- CIRSx2/settings.py
+++ CIRSx2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
@@ -133,14 +133,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+"""
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 """
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-"""
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
